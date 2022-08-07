ecaf36362b7d1659cb6912740b6afd3ddf48eefb
CjS77 <CjS77@users.noreply.github.com>
2022-08-07 11:39:30 +0100

    chore: move static data to own module
    lets us use the same data for unit and async tests easily

--- github/src/models/labels.rs
+++ github/src/models/labels.rs
@@ -15,56 +15,8 @@ pub struct Label {
 #[cfg(test)]
 mod test {
     use crate::models::labels::Label;
+    use crate::models::static_data::labels::LABELS;
 
-    const LABELS: &str = r#"
-[
-    {
-        "id": 1189345686,
-        "node_id": "MDU6TGFiZWwxMTg5MzQ1Njg2",
-        "url": "https://api.github.com/repos/tari-project/tari/labels/in%20progress",
-        "name": "in progress",
-        "color": "ededed",
-        "default": false,
-        "description": null
-    },
-    {
-        "id": 1390045826,
-        "node_id": "MDU6TGFiZWwxMzkwMDQ1ODI2",
-        "url": "https://api.github.com/repos/tari-project/tari/labels/Integration%20test",
-        "name": "Integration test",
-        "color": "f435f9",
-        "default": false,
-        "description": ""
-    },
-    {
-        "id": 957513340,
-        "node_id": "MDU6TGFiZWw5NTc1MTMzNDA=",
-        "url": "https://api.github.com/repos/tari-project/tari/labels/invalid",
-        "name": "invalid",
-        "color": "e4e669",
-        "default": true,
-        "description": "This doesn't seem right"
-    },
-    {
-        "id": 4274976216,
-        "node_id": "LA_kwDOCCIzW87-zvXY",
-        "url": "https://api.github.com/repos/tari-project/tari/labels/javascript",
-        "name": "javascript",
-        "color": "168700",
-        "default": false,
-        "description": "Pull requests that update Javascript code"
-    },
-    {
-        "id": 2024296734,
-        "node_id": "MDU6TGFiZWwyMDI0Mjk2NzM0",
-        "url": "https://api.github.com/repos/tari-project/tari/labels/Known-Issue",
-        "name": "Known-Issue",
-        "color": "bfdadc",
-        "default": false,
-        "description": "Issue that has already been discovered"
-    }
-]
-    "#;
     #[test]
     fn deserialize_labels() {
         let labels: Vec<Label> = serde_json::from_str(LABELS).unwrap();
