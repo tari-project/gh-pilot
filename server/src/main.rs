6334a2fe52c16f5f1a6d7f19055a90a5a3a09f45
CjS77 <CjS77@users.noreply.github.com>
2022-10-11 14:24:05 +0100

    chore: add startup log
    Log the host and port info

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
 use ghp_server::{config::ServerConfig, server::run_server};
+use log::info;
 
 #[actix_web::main]
 async fn main() {
     env_logger::init();
     let config = ServerConfig::new("127.0.0.1", 8330);
+    info!("🚀 Starting server on {}:{}", config.host, config.port);
     match run_server(config).await {
         Ok(_) => println!("Bye!"),
         Err(e) => eprintln!("{e}"),
