f361fe54f2a6d5c831a8c0b0e41d04a2d37de011
CjS77 <CjS77@users.noreply.github.com>
2022-10-12 12:45:20 +0100

    feat: add statuscheck predicate
    PRovides a new predicate for RuleBuilder to trigger on a successful
    completion of the status check suite
    
    ```
    RuleBuilder::new("AutoMerge™")
        .when(StatusCheck::suite_success())
        .execute(Actions::auto_merge().build())
        .submit()
    ```

--- server/src/predicates/mod.rs
+++ server/src/predicates/mod.rs
@@ -1,5 +1,7 @@
 mod pull_request;
 mod pull_request_comment;
+mod status_checks;
 
 pub use pull_request::PullRequest;
 pub use pull_request_comment::PullRequestComment;
+pub use status_checks::StatusCheck;
