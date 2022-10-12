0e23300088ca67da65e9f81c137b3d615e7263d8
Cayle Sharrock <CjS77@users.noreply.github.com>
2022-10-12 13:36:02 +0100

    feat: add statuscheck predicate  (#32)
    * feat: add check_suite event
    
    adds deserialization support for the check_suite github event.
    This insludes the App struct and app permissions which are inlded in the
    event payload.
    
    The Github docs appear to be out of date here, since there were several
    permissions that were not included in the API docs -- I've added them to
    the struct, but thi smay lead to deserialization bugs in future.
    
    * feat: add statuscheck predicate
    
    PRovides a new predicate for RuleBuilder to trigger on a successful
    completion of the status check suite
    
    ```
    RuleBuilder::new("AutoMerge™")
        .when(StatusCheck::suite_success())
        .execute(Actions::auto_merge().build())
        .submit()
    ```
    
    * feat: add statuscheck predicate
    
    PRovides a new predicate for RuleBuilder to trigger on a successful
    completion of the status check suite
    
    ```
    RuleBuilder::new("AutoMerge™")
        .when(StatusCheck::suite_success())
        .execute(Actions::auto_merge().build())
        .submit()
    ```
    
    * fix: check run pr objects
    
    The list of pull requests referenced in the check_suite object are not
    full PullRequest objects, so we needed to define a new struct to capture
    this.
    
    The Git references in that object are also slightly different, so also
    added the GitReferenceShort struct.
    
    The failing event now passes in a new regression test.

---
+++ server/src/predicates/status_checks.rs
@@ -0,0 +1,47 @@
+use github_pilot_api::webhooks::{CheckSuiteConclusion, CheckSuiteStatus, GithubEvent};
+
+use crate::{pub_sub::GithubEventMessage, rules::RulePredicate};
+
+pub struct StatusCheck {
+    trigger: StatusCheckPredicate,
+}
+
+enum StatusCheckPredicate {
+    CheckSuiteSuccess,
+}
+
+impl StatusCheck {
+    /// Triggers when a Status Check Suite completes successfully.
+    pub fn suite_success() -> Self {
+        Self {
+            trigger: StatusCheckPredicate::CheckSuiteSuccess,
+        }
+    }
+}
+
+impl RulePredicate for StatusCheck {
+    fn matches(&self, event: &GithubEventMessage) -> bool {
+        match (event.event(), &self.trigger) {
+            (GithubEvent::CheckSuiteEvent(ev), StatusCheckPredicate::CheckSuiteSuccess) => {
+                matches!(ev.check_suite.status, Some(CheckSuiteStatus::Completed)) &&
+                    matches!(ev.check_suite.conclusion, Some(CheckSuiteConclusion::Success))
+            },
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::predicates::status_checks::StatusCheck;
+
+    #[test]
+    fn check_suite_success() {
+        let data = include_str!("../../test-data/check_suite_event1.json");
+        let event = GithubEvent::try_from_webhook_info("check_suite", data).unwrap();
+        let msg = GithubEventMessage::new("check-suite", event);
+        let predicate = StatusCheck::suite_success();
+        assert!(predicate.matches(&msg));
+    }
+}
