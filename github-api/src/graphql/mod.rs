91d240e5be559ac73e330a84fbe7f6c05a8779af
Cayle Sharrock <CjS77@users.noreply.github.com>
2022-10-18 22:53:55 +0100

    fix: handle status checks better (#52)
    Issues:
    The status roll up result sometimes returns a false negative
    External tests (e.g. CircleCI) were not included in the results
    Repeated tests were being double-counted.
    
    Now:
    Update grpahQL query to return external tests too
    Only use status rollup if it returns success
    Refactor RunStatus to hold a HashMap of only the latest result of each
    test
    Co-erce GHA and external tests into the same RunStatus struct
    Provide helper functions in RunStatus to clean up the MergeAction code
    Tidy up nested From impl mess a bit

--- github-api/src/graphql/mod.rs
+++ github-api/src/graphql/mod.rs
@@ -3,4 +3,4 @@ pub mod review_counts;
 pub mod run_status;
 
 pub use pr_comments::{Comment, CommentThread, PullRequestComments};
-pub use run_status::{CheckRunStatus, RunStatus};
+pub use run_status::{CheckResult, CheckRunStatus, RunStatus};
--- github-api/src/graphql/run_status.rs
+++ github-api/src/graphql/run_status.rs
@@ -1,7 +1,7 @@
-use std::slice::Iter;
+use std::collections::{hash_map::Values, HashMap};
 
 use graphql_client::GraphQLQuery;
-use log::warn;
+use serde::Serialize;
 
 use crate::models::{DateTime, Url};
 
@@ -17,18 +17,27 @@ type URI = Url;
 )]
 pub struct CheckRunStatusQL;
 
+#[derive(Debug, Clone)]
 pub struct CheckRunStatus {
     commit_url: Url,
     committed_at: DateTime,
     overall_status: Option<check_run_status_ql::StatusState>,
-    checks: Vec<RunStatus>,
+    checks: HashMap<String, RunStatus>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub enum CheckResult {
+    ERROR,
+    FAILURE,
+    PENDING,
+    SUCCESS,
+}
+
+#[derive(Debug, Clone)]
 pub struct RunStatus {
     pub name: String,
     pub completed_at: DateTime,
-    pub result: check_run_status_ql::CheckConclusionState,
-    pub status: check_run_status_ql::CheckStatusState,
+    pub result: CheckResult,
     pub is_required: bool,
 }
 
@@ -38,10 +47,15 @@ impl Default for CheckRunStatus {
             commit_url: Url::from(""),
             committed_at: DateTime::default(),
             overall_status: None,
-            checks: vec![],
+            checks: HashMap::new(),
         }
     }
 }
+
+use check_run_status_ql::{
+    CheckRunStatusQlRepositoryPullRequestCommitsNodesCommitCheckSuitesNodesCheckRunsNodes as CheckRunSuite,
+    CheckRunStatusQlRepositoryPullRequestCommitsNodesCommitStatusContexts as StatusContext,
+};
 
 impl CheckRunStatus {
     pub fn commit_url(&self) -> &Url {
@@ -56,8 +70,88 @@ impl CheckRunStatus {
         self.overall_status.as_ref()
     }
 
-    pub fn checks(&self) -> Iter<RunStatus> {
-        self.checks.iter()
+    pub fn checks(&self) -> Values<String, RunStatus> {
+        self.checks.values()
+    }
+
+    pub fn totals(&self) -> (usize, usize, usize) {
+        let (required, passed) = self.checks().fold((0usize, 0usize), |(required, success), check| {
+            if check.is_required {
+                (
+                    required + 1,
+                    success + usize::from(check.result == CheckResult::SUCCESS),
+                )
+            } else {
+                (required, success)
+            }
+        });
+        (self.checks.len(), required, passed)
+    }
+
+    pub fn has_passed(&self) -> bool {
+        if matches!(self.overall_status, Some(check_run_status_ql::StatusState::SUCCESS)) {
+            return true;
+        }
+        self.checks
+            .values()
+            .filter(|&check| check.is_required)
+            .all(|check| matches!(check.result, CheckResult::SUCCESS))
+    }
+}
+
+impl From<check_run_status_ql::StatusState> for CheckResult {
+    fn from(state: check_run_status_ql::StatusState) -> Self {
+        match state {
+            check_run_status_ql::StatusState::FAILURE => CheckResult::FAILURE,
+            check_run_status_ql::StatusState::PENDING => CheckResult::PENDING,
+            check_run_status_ql::StatusState::EXPECTED => CheckResult::PENDING,
+            check_run_status_ql::StatusState::SUCCESS => CheckResult::SUCCESS,
+            check_run_status_ql::StatusState::ERROR => CheckResult::ERROR,
+            check_run_status_ql::StatusState::Other(_) => CheckResult::ERROR,
+        }
+    }
+}
+
+impl From<check_run_status_ql::CheckConclusionState> for CheckResult {
+    fn from(state: check_run_status_ql::CheckConclusionState) -> Self {
+        match state {
+            check_run_status_ql::CheckConclusionState::ACTION_REQUIRED => CheckResult::FAILURE,
+            check_run_status_ql::CheckConclusionState::CANCELLED => CheckResult::ERROR,
+            check_run_status_ql::CheckConclusionState::FAILURE => CheckResult::FAILURE,
+            check_run_status_ql::CheckConclusionState::NEUTRAL => CheckResult::FAILURE,
+            check_run_status_ql::CheckConclusionState::SKIPPED => CheckResult::ERROR,
+            check_run_status_ql::CheckConclusionState::STALE => CheckResult::ERROR,
+            check_run_status_ql::CheckConclusionState::STARTUP_FAILURE => CheckResult::ERROR,
+            check_run_status_ql::CheckConclusionState::SUCCESS => CheckResult::SUCCESS,
+            check_run_status_ql::CheckConclusionState::TIMED_OUT => CheckResult::ERROR,
+            check_run_status_ql::CheckConclusionState::Other(_) => CheckResult::ERROR,
+        }
+    }
+}
+
+impl From<CheckRunSuite> for RunStatus {
+    fn from(suite: CheckRunSuite) -> Self {
+        Self {
+            name: suite.name,
+            completed_at: suite.completed_at.unwrap_or_default(),
+            result: CheckResult::from(
+                suite
+                    .conclusion
+                    .unwrap_or(check_run_status_ql::CheckConclusionState::SKIPPED),
+            ),
+            is_required: suite.is_required,
+        }
+    }
+}
+
+impl From<StatusContext> for RunStatus {
+    fn from(s: StatusContext) -> Self {
+        Self {
+            name: s.context,
+            completed_at: s.created_at,
+            result: CheckResult::from(s.state),
+            is_required: s.is_required,
+        }
     }
 }
 
@@ -84,62 +178,40 @@ impl From<check_run_status_ql::ResponseData> for CheckRunStatus {
             committed_date: committed_at,
             status_check_rollup,
             check_suites,
+            status,
         } = match statuses.into_iter().next() {
             Some(Some(CN { commit: c })) => c,
             _ => return CheckRunStatus::default(),
         };
-        let overall_status = status_check_rollup.map(|s| s.state);
-        let checks = match check_suites {
-            Some(cs) => cs
-                .nodes
-                .into_iter()
-                .flatten()
-                .flatten()
-                .flat_map(|node| {
-                    let app_name = node
-                        .app
-                        .map(|app| app.name)
-                        .unwrap_or_else(|| "App name not provided".to_string());
-                    match node.check_runs {
-                        Some(cr) => {
-                            let run_count = cr.total_count as usize;
-                            let fetched = cr.nodes.iter().flatten().flatten().count();
-                            if run_count != fetched {
-                                warn!(
-                                    "There are {run_count} check runs for {app_name}, but only {fetched} were \
-                                     fetched. File a bug report saying we need proper pagination in the run_status \
-                                     query code."
-                                );
-                            }
-                            cr.nodes
-                                .into_iter()
-                                .flatten()
-                                .flatten()
-                                .map(|run| {
-                                    let name = run.name;
-                                    let completed_at = run.completed_at.unwrap_or_default();
-                                    let result = run.conclusion.unwrap_or_else(|| {
-                                        check_run_status_ql::CheckConclusionState::Other("unknown".to_string())
-                                    });
-                                    let status = run.status;
-                                    let is_required = run.is_required;
-                                    RunStatus {
-                                        name,
-                                        completed_at,
-                                        result,
-                                        status,
-                                        is_required,
-                                    }
-                                })
-                                .collect::<Vec<RunStatus>>()
-                        },
-                        None => vec![],
-                    }
-                })
-                .collect(),
-            None => vec![],
-        };
 
+        let overall_status = status_check_rollup.map(|s| s.state);
+        let checks = check_suites
+            .into_iter()
+            .flat_map(|cs| {
+                cs.nodes.into_iter().flatten().flatten().flat_map(|node| {
+                    node.check_runs
+                        .map(|cr| cr.nodes.into_iter().flatten().flatten().map(RunStatus::from))
+                })
+            })
+            .flatten();
+
+        let statuses = status
+            .into_iter()
+            .flat_map(|s| s.contexts.into_iter().map(RunStatus::from));
+
+        let checks = checks
+            .chain(statuses)
+            .fold(HashMap::new(), |mut map: HashMap<String, RunStatus>, status| {
+                let insert = if let Some(existing) = map.get(&status.name) {
+                    status.completed_at > existing.completed_at
+                } else {
+                    true
+                };
+                if insert {
+                    map.insert(status.name.clone(), status);
+                }
+                map
+            });
         CheckRunStatus {
             commit_url,
             committed_at,
@@ -151,36 +223,40 @@ impl From<check_run_status_ql::ResponseData> for CheckRunStatus {
 
 #[cfg(test)]
 mod test {
-    use super::check_run_status_ql::{CheckConclusionState, CheckStatusState, ResponseData, StatusState};
-    use crate::graphql::CheckRunStatus;
+    use super::check_run_status_ql::{ResponseData, StatusState};
+    use crate::graphql::{run_status::CheckResult, CheckRunStatus};
 
     #[test]
     fn deserialization() {
         let json = include_str!("data/sample_check_run.json");
         let res: ResponseData = serde_json::from_str(json).unwrap();
         let status = CheckRunStatus::from(res);
-        assert_eq!(status.overall_status, Some(StatusState::SUCCESS));
+        assert_eq!(status.overall_status, Some(StatusState::FAILURE));
         assert_eq!(
             status.commit_url.as_ref(),
-            "https://github.com/tari-project/gh-pilot/commit/c13501c51f6a3f5183f5921aa89d68110a29f0f4"
+            "https://github.com/tari-project/tari/commit/eebba41dd22dbcc8ce94b86a0c25cf8d483dc3d7"
         );
-        assert_eq!(status.committed_at.to_string(), "2022-09-20T15:30:05Z");
-        assert_eq!(status.checks.len(), 5);
-        let check = &status.checks[0];
-        assert_eq!(check.name, "clippy_check");
-        assert_eq!(check.completed_at.to_string(), "2022-09-20T15:35:54Z");
+        assert_eq!(status.committed_at.to_string(), "2022-10-18T11:36:33Z");
+        assert_eq!(status.checks.len(), 10);
+
+        let check = status.checks.get("check-title").unwrap();
+        assert_eq!(check.name, "check-title");
+        assert_eq!(check.completed_at.to_string(), "2022-10-18T15:22:48Z");
+        assert!(check.is_required);
+        assert_eq!(check.result, CheckResult::SUCCESS);
+
+        let check = status.checks.get("check stable").unwrap();
+        assert_eq!(check.name, "check stable");
+        assert_eq!(check.result, CheckResult::SUCCESS);
+
+        let check = status.checks.get("ci/circleci: run-ffi-integration-tests").unwrap();
+        assert_eq!(check.name, "ci/circleci: run-ffi-integration-tests");
+        assert_eq!(check.result, CheckResult::FAILURE);
         assert!(!check.is_required);
-        assert_eq!(check.result, CheckConclusionState::SUCCESS);
-        assert_eq!(check.status, CheckStatusState::COMPLETED);
-        let check = &status.checks[1];
-        assert_eq!(check.name, "check-title");
-        assert_eq!(check.result, CheckConclusionState::SUCCESS);
-        assert_eq!(check.status, CheckStatusState::COMPLETED);
-        let check = &status.checks[2];
-        assert_eq!(check.name, "cargo test (stable)");
-        let check = &status.checks[3];
-        assert_eq!(check.name, "cargo test (nightly-2022-09-13)");
-        let check = &status.checks[4];
-        assert_eq!(check.name, "check-title");
+
+        let check = status.checks.get("DeepSource: Rust").unwrap();
+        assert_eq!(check.name, "DeepSource: Rust");
+        assert_eq!(check.result, CheckResult::SUCCESS);
+        assert!(!check.is_required);
     }
 }
