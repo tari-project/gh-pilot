85e97e5ff687938f76e1606eedb5f5eaa46c9c2a
Cayle Sharrock <CjS77@users.noreply.github.com>
2022-11-29 17:41:40 +0000

    feat: github provider fetches users (#55)
    Implements the stubbed UserProvider trait method on GithubProvidder
    
    ```
    $ ./target/debug/ghp-cli user
    [2022-11-29T17:09:42Z INFO  ghp_cli] 🚀 Ignition! Launching Github Pilot in Authenticated Mode.
    ✔ 👨 Github handle: …
    ┌────────────────────────────────────────────────┐
    │ User name   CjS77                              │
    │ Type        Cayle Sharrock                     │
    │ URL         https://api.github.com/users/CjS77 │
    └────────────────────────────────────────────────┘
    ```

---
+++ github-api/src/api/profile.rs
@@ -0,0 +1,29 @@
+use crate::{
+    api::{ClientProxy, GithubApiError},
+    models::SimpleUser,
+};
+
+pub struct UserRequest {
+    username: String,
+}
+
+impl UserRequest {
+    pub fn new<S: Into<String>>(username: S) -> Self {
+        Self {
+            username: username.into(),
+        }
+    }
+
+    pub fn get_path(&self) -> String {
+        format!("/users/{}", self.username)
+    }
+
+    pub async fn fetch(&self, proxy: &ClientProxy) -> Result<Option<SimpleUser>, GithubApiError> {
+        let req = proxy.get(self.get_path().as_str(), false);
+        match proxy.send::<SimpleUser>(req).await {
+            Ok(user) => Ok(Some(user)),
+            Err(GithubApiError::NotFound(_)) => Ok(None),
+            Err(e) => Err(e),
+        }
+    }
+}
